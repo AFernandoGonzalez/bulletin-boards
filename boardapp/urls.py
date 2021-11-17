d64bb0a9f1bf3a1b12057babbecbf7f53e0e55f1
Fernando <ferg.developer@gmail.com>
2021-11-17 12:26:38 -0500

    Basic Studetn UI Done

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -5,5 +5,8 @@ app_name = 'boardapp'
 
 
 urlpatterns = [
-    path('', views.home_view, name='home_view')
+    path('', views.home_view, name='home_view'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('boards/', views.boardlist, name='boards'),
+    path('board/1/', views.boarddetail, name='board-detail'),
 ]
--- boardapp/views.py
+++ boardapp/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home_view(request):
     return render (request, 'boardapp/home.html')
+
+def dashboard(request):
+    return render (request, 'boardapp/dashboard.html')
+
+def boardlist(request):
+    return render (request, 'boardapp/boardlist.html')
+
+def boarddetail(request):
+    return render (request, 'boardapp/boarddetails.html')
