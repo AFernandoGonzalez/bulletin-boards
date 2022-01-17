ff43fb3407b8d12900530800c6536665cc8f750a
Fernando <ferg.developer@gmail.com>
2022-01-17 12:15:26 -0500

    flyer detail page updated

--- boardapp/forms.py
+++ boardapp/forms.py
@@ -18,26 +18,18 @@ class AddFlyerForm(forms.ModelForm):
     date_posted = forms.DateField(widget=DateInput)
     class Meta:
         model = Flyer
-        fields = ('board', 'office', 'name', 'image', 'date_posted', 'due_date', 'flyer_creator', 'flyer_edited' , 'date_posted' )
-    
-  
+        fields = ('added_by', 'board', 'office', 'name', 'image', 'removed', 'date_posted', 'due_date', 'flyer_creator', 'flyer_edited' , 'date_posted' )
+
     board = forms.ModelMultipleChoiceField(
         queryset=Board.objects.all(),
         widget=forms.CheckboxSelectMultiple
     )
 
-    flyer_creator = forms.ModelMultipleChoiceField(
-        queryset=Flyer.objects.all(),
-        widget=forms.CheckboxSelectMultiple
-    )
-
-    flyer_edited = forms.ModelMultipleChoiceField(
-        queryset=Board.objects.all(),
-        widget=forms.CheckboxSelectMultiple
-    )
+  
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         self.fields['board'].widget.attrs.update(
             {'class': 'boards-checkbox', 'placeholder': 'Choose Board'})
         
@@ -45,20 +37,16 @@ class AddFlyerForm(forms.ModelForm):
             {'class': 'select-office', 'placeholder': 'Select Office'})
         
         self.fields['name'].widget.attrs.update(
-            {'class': 'select-name', 'placeholder': 'Select Office'})
+            {'class': 'select-name', 'placeholder': 'Title of Flyer'})
         
         self.fields['image'].widget.attrs.update(
-            {'class': 'select-image', 'placeholder': 'Select Office'})
+            {'class': 'select-image', 'placeholder': ''})
         
         self.fields['date_posted'].widget.attrs.update(
-            {'class': 'select-posted_date', 'placeholder': 'Select Office'})
+            {'class': 'select-posted_date', 'placeholder': ''})
         
         self.fields['due_date'].widget.attrs.update(
-            {'class': 'select-due_date', 'placeholder': 'Select Office'})
-
-    
-
-   
+            {'class': 'select-due_date', 'placeholder': ''})
 
 
   
